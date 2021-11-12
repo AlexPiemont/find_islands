4704d4981529e756219f7acb02c4a6640aae1afe
AlexPiemont <piemont.alexander@gmail.com>
2021-11-12 12:44:43 +0100

    included direction in visited check

--- main.go
+++ main.go
@@ -2,12 +2,36 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
+
+type direction struct {
+	Right bool
+	Up    bool
+	Left  bool
+	Down  bool
+}
 
 type imgMap struct {
 	Inmap   [][]int
-	Visited [][]int
+	Visited [][]direction
 	Outmap  [][]int
+}
+
+func (mp *imgMap) toStringCompare() string {
+	resStr := "   Input           Output \n"
+
+	for i := 0; i < len(mp.Inmap); i++ {
+		inStr := ""
+		visStr := ""
+		outStr := ""
+		for j := 0; j < len(mp.Inmap[i]); j++ {
+			inStr = inStr + " " + strconv.Itoa(mp.Inmap[i][j])
+			outStr = outStr + " " + strconv.Itoa(mp.Outmap[i][j])
+		}
+		resStr = resStr + inStr + "    " + visStr + "    " + outStr + "\n"
+	}
+	return resStr
 }
 
 func makeImgMap(input [][]int) imgMap {
@@ -17,38 +41,86 @@ func makeImgMap(input [][]int) imgMap {
 	for i := range ret.Outmap {
 		ret.Outmap[i] = make([]int, len(input[i]))
 	}
-	ret.Visited = make([][]int, len(input))
+	ret.Visited = make([][]direction, len(input))
 	for i := range ret.Visited {
-		ret.Visited[i] = make([]int, len(input[i]))
+		ret.Visited[i] = make([]direction, len(input[i]))
 	}
 	return ret
 }
-func (i *imgMap) processNode(x, y int) {
-	fmt.Printf("currently at position %d, %d \n", x, y)
-	i.Visited[x][y] = 1
-	fmt.Printf("visited: %d", i.Visited)
+func (i *imgMap) isEdge(x, y int) bool {
+	if x == 0 || y == 0 || x == len(i.Inmap[0])-1 || y == len(i.Inmap)-1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (i *imgMap) inBounds(x, y int) bool {
+	if x >= 0 && y >= 0 && x < len(i.Inmap[0]) && y < len(i.Inmap) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (d *direction) add(inD int) {
+	switch inD {
+	case 0:
+		d.Right = true
+	case 1:
+		d.Up = true
+	case 2:
+		d.Left = true
+	case 3:
+		d.Down = true
+	default:
+		break
+	}
+}
+
+func (d *direction) checkVisited(inD int) bool {
+	switch inD {
+	case 0:
+		return d.Right
+	case 1:
+		return d.Up
+	case 2:
+		return d.Left
+	case 3:
+		return d.Down
+	default:
+		return false
+	}
+}
+
+func (i *imgMap) processNode(x, y int, fromOne bool, fromDir int) {
+	//fromDir schema {up,down,left,right}
+	//fmt.Printf("currently at position %d, %d \n", x, y)
+	i.Visited[x][y].add(fromDir)
+	//fmt.Printf("visited: %d", i.Visited)
 	//fmt.Print("Press 'Enter' to continue...")
 	//bufio.NewReader(os.Stdin).ReadBytes('\n')
 	dirs := [][]int{
-		{-1, 0},
-		{0, 1},
-		{1, 0},
-		{0, -1},
+		{-1, 0}, //from right
+		{0, 1},  //from up
+		{1, 0},  //from left
+		{0, -1}, //from down
 	}
-	if i.Inmap[x][y] == 1 || x == 0 || y == 0 || x == len(i.Inmap[0])-1 || y == len(i.Inmap)-1 {
+	isOne := i.Inmap[x][y] == 1
+
+	if i.isEdge(x, y) || fromOne {
 		i.Outmap[x][y] = i.Inmap[x][y]
 		for j := 0; j < len(dirs); j++ {
-			if x+dirs[j][0] >= 0 && y+dirs[j][1] >= 0 && x+dirs[j][0] < len(i.Inmap[0]) && y+dirs[j][1] < len(i.Inmap) {
-				if i.Visited[x+dirs[j][0]][y+dirs[j][1]] == 0 {
-					i.processNode(x+dirs[j][0], y+dirs[j][1])
-				}
+			if i.inBounds(x+dirs[j][0], y+dirs[j][1]) && !i.Visited[x+dirs[j][0]][y+dirs[j][1]].checkVisited(j) {
+				i.processNode(x+dirs[j][0], y+dirs[j][1], isOne, j)
 			}
 		}
 	}
+
 }
 func removeIslands(input [][]int) [][]int {
 	mp := makeImgMap(input)
-	mp.processNode(0, 0)
+	mp.processNode(0, 0, false, 4)
 	return mp.Outmap
 }
 func main() {
@@ -60,11 +132,8 @@ func main() {
 		{1, 1, 0, 0, 1},
 	}
 	mp := makeImgMap(queue)
-	fmt.Println(mp.Visited)
-	fmt.Println(mp.Outmap)
-	mp.processNode(0, 0)
-	fmt.Println(mp.Inmap)
-	fmt.Println(mp.Visited)
-	fmt.Println(mp.Outmap)
+	fmt.Println(mp.toStringCompare())
+	mp.processNode(0, 0, false, 4)
+	fmt.Println(mp.toStringCompare())
 
 }
--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func FindIslands(t *testing.T) {
+func TestFindIslands(t *testing.T) {
 	input := [][]int{
 		{0, 1, 1, 0, 0},
 		{1, 0, 1, 1, 0},
@@ -23,6 +23,26 @@ func FindIslands(t *testing.T) {
 	output := removeIslands(input)
 	if !reflect.DeepEqual(output, expected) {
 		t.Fatalf(`Expected: %v Got: %v`, expected, output)
+	}
+}
+func TestCenterIsland(t *testing.T) {
+	input := [][]int{
+		{1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 1},
+		{1, 0, 1, 0, 1},
+		{1, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1},
+	}
+	expected := [][]int{
+		{1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 1},
+		{1, 0, 0, 0, 1},
+		{1, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1},
+	}
+	output := removeIslands(input)
+	if !reflect.DeepEqual(output, expected) {
+		t.Fatalf("\nExpected: %v \nGot: %v", expected, output)
 	}
 }
 
